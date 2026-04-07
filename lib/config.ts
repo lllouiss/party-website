f13c08e7fb1d425ea0bea93de8f9d7987d0a4ec9
Louis Wenk <lw@hli.ch>
2026-04-07 17:15:47 +0200

    feat: update event price

--- lib/config.ts
+++ lib/config.ts
@@ -4,7 +4,7 @@ export const PARTY_CONFIG = {
   time: "20:00 Uhr",
   location: "Waldhaus Büttikon",
   eventDate: new Date("2026-06-26T20:00:00"),
-  price: 20,
+  price: 10,
   twintNumber: "+41 76 720 92 32",
   maxGuests: 500,
   mapsEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3640.9160584092347!2d8.27671119547762!3d47.327435204909676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479010f2db99ddd1%3A0xe554c3b1bf8130ae!2sWaldhaus%20B%C3%BCttikon!5e1!3m2!1sde!2sch!4v1775563872266!5m2!1sde!2sch",
